5f6a09b00d8a8cd2937e683d4ef28ea5bb077ff8
yanponka <yan_ponka_@hotmail.com>
2017-02-13 20:49:50 -0300

    Pedido Web

---
+++ pedidoweb/src/main/java/br/comlarimaia/pedidoweb/App.java
@@ -0,0 +1,13 @@
+package br.comlarimaia.pedidoweb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
